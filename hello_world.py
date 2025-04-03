dfe9bf348bb87b73e6c2ab20c5fd9a4d6f57ee91
berkhee123 <berkhee123@gmail.com>
2025-04-04 05:47:45 +0800

    nas

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+print("Hello world!") 
+print("Таны нэрийг хэн гэдэг вэ?") 
+name = input("")
+print(f"Сайн байна уу, намайг {name} гэдэг") 
+print("Таны нас хэд вэ ?") 
+age = int(input())
+print(f" Би {age} настай ") 
+result = 2025 + 100 - age 
+print(result)
+#a = 1
+#b = 2
+#a + b
+#print( a + b )
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
