62c9dcd3a88fca721dd8117e41f4a2c6db7854d3
berkhee123 <berkhee123@gmail.com>
2025-04-04 20:00:31 +0800

    daalgavar 1

--- hello_world.py
+++ hello_world.py
@@ -3,10 +3,23 @@ print("Таны нэрийг хэн гэдэг вэ?")
 name = input("")
 print(f"Сайн байна уу, намайг {name} гэдэг") 
 print("Таны нас хэд вэ ?") 
-age = int(input())
-print(f" Би {age} настай ") 
-result = 2025 + 100 - age 
-print(result)
+try:
+    age = int(input())  # Тоон утга эсэхийг шалгах
+    print(f"Би {age} настай")
+    
+    result = 2025 + 100 - age  # ??? онд 100 нас хүрэхийг тооцолов 
+    print(f"Та {result} онд 100 нас хүрнэ")  
+
+except ValueError:  
+    print("Та зөвхөн тоон утга оруулна уу!") # int биш байх юм бол хариулах алдааны мсж 
+
+
+
+
+
+       
+       
+
 #a = 1
 #b = 2
 #a + b
